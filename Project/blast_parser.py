a2542eaa579479255b0cf0375319d1be0e65e5f8
mac40 <marcobgg4@gmail.com>
2019-01-15 12:29:58 +0100

    code refactor

--- Project/blast_parser.py
+++ Project/blast_parser.py
@@ -1,36 +1,60 @@
 '''
-Transform the xml output of a blast query into a fasta file containing the proteins from uniprotkb
-INPUT: xml file containing blast output
-OUTPUT: fasta file containing proteins matched by blast
+Transform the xml output of a blast query into a fasta file containing the proteins from uniprotkb\n
+INPUT: xml file containing blast output\n
+OUTPUT: fasta file containing proteins matched by blast\n
 '''
 
+import os
+import sys
 import urllib.request
 import xml.etree.ElementTree as ET
 
+FILE_POS = os.path.dirname(os.path.abspath(__file__))
 
-def parse_blast(xml_file, output_file_name):
+def get_protein_list(xml_file):
     '''
-    parse blast
-    INPUT: xml_file_name, outputfilename
-    OUTPUT: fasta file
+    parse blast\n
+    INPUT: xml_file_name\n
+    RETURN: list of proteins\n
     '''
     tree = ET.parse(xml_file)
     root = tree.getroot()
-    base_url = "https://www.uniprot.org/uniprot/"
-    extention = ".fasta"
-    with open(output_file_name, "w") as output:
-        for sssr in root.findall('{http://www.ebi.ac.uk/schema}SequenceSimilaritySearchResult'):
-            for hits in sssr:
-                hit_list = []
-                for hit in hits:
-                    protein = hit.get('ac')
-                    if protein not in hit_list:
-                        hit_list.append(protein)
-                        url = base_url + protein + extention
-                        protein_fasta = urllib.request.urlopen(url).read()
-                        output.write(protein_fasta.decode("utf-8"))
+    for sssr in root.findall('{http://www.ebi.ac.uk/schema}SequenceSimilaritySearchResult'):
+        for hits in sssr:
+            hit_list = []
+            for hit in hits:
+                protein = hit.get('ac')
+                if protein not in hit_list:
+                    hit_list.append(protein)
+    return hit_list
+
+
+def query_uniprotkb(proteins, output_file_name):
+    '''
+    query uniprotkb for sequences given a protein list and output them in a fasta file\n
+    INPUT: protein list, name of outputfile\n
+    OUTPUT: fasta file with proteins sequences\n
+    '''
+    with open(output_file_name, 'w') as output:
+        base_url = 'https://www.uniprot.org/uniprot/'
+        extention = '.fasta'
+        for protein in proteins:
+            url = base_url + protein + extention
+            protein_fasta = urllib.request.urlopen(url).read()
+            output.write(protein_fasta.decode("utf-8"))
 
 
 if __name__ == "__main__":
-    parse_blast('ncbiblast-R20190112-174458-0413-89062590-p2m.xml',
-                'blast.fasta')
+    if len(sys.argv) == 1:
+        print('no parameters specified (type python blast_parser.py -h for help)')
+        sys.exit()
+    elif str(sys.argv[1]) == '-h':
+        print('python blast_parser.py xml_file output_file_name')
+        sys.exit()
+    elif len(sys.argv) < 3:
+        print('missing parameters')
+        sys.exit()
+    else:
+        PROTEIN_LIST = get_protein_list(sys.argv[1])
+        query_uniprotkb(PROTEIN_LIST,
+                        '{}/parsed_blast_results/{}.fasta'.format(FILE_POS, sys.argv[2]))
