2bb1bbf21b2ffe61ed87cf0f78e1678c2dd508ec
mac40 <marcobgg4@gmail.com>
2019-01-16 10:43:27 +0100

    cd hit results/cluster divider

---
+++ Project/cd_hit_separator.py
@@ -0,0 +1,25 @@
+'''
+split clusters in single fasta files
+'''
+
+import re
+import sys
+import urllib.request
+
+
+if __name__ == "__main__":
+    with open(sys.argv[1], 'r') as infile:
+        BASE_URL = 'https://www.uniprot.org/uniprot/'
+        EXTENTION = '.fasta'
+        for line in infile:
+            if re.match(r'>Cluster\s[0-9]*', line):
+                if int(line[9:]) >= 1:
+                    cluster.close()
+                cluster = open('cd_hit_results/clusters/{}.fasta'.format(line[1:-1]), 'w')
+            else:
+                regex = re.search(r'[0-9]*\s[0-9]*aa,\s>[a-z]{2}\|(.*)\|.*', line)
+                protein = regex.group(1)
+                url = BASE_URL + protein + EXTENTION
+                protein_fasta = urllib.request.urlopen(url).read()
+                cluster.write(protein_fasta.decode("utf-8"))
+    cluster.close()
