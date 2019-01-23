a625c98d9e60b0916fecf42b8d6d75db84b0de44
mac40 <marcobgg4@gmail.com>
2019-01-23 17:15:22 +0100

    __pycache__ added to gitignore

---
+++ Project/main.py
@@ -0,0 +1,9 @@
+'''
+main python file
+'''
+
+import blast_parser as bp
+
+if __name__ == "__main__":
+    PROTEIN_LIST = bp.txt_to_protein_list("./1_blast_results/uniref90_ids.txt")
+    bp.query_uniprotkb(PROTEIN_LIST, './2_parsed_blast_results/uniref90_blast_100.fasta', 100)
