730fbd4a0566ed8529d56be7ad995847180c40dc
mac40 <marcobgg4@gmail.com>
2019-01-24 15:14:57 +0100

    clustalw_analyzer updated to work with bash input

--- Project/clustalw_analyzer.py
+++ Project/clustalw_analyzer.py
@@ -3,10 +3,13 @@ defining function to modify clustal omega results in order to remove columns
 under certain threshold of conservation
 '''
 
+import sys
 from Bio import AlignIO
 
 
-ALIGN = AlignIO.read("./3_clustal_omega_results/muscle_uniref90_100.clw", "clustal")
+ALIGN = AlignIO.read(sys.argv[1], "clustal")
+
+THRESHOLD = sys.argv[2]
 
 REMOVABLE_COLS = []
 
@@ -15,10 +18,12 @@ for col in range(0, len(ALIGN[0])):
     for el in ALIGN[:, col]:
         if el == '-':
             gaps += 1
-    if gaps > 20:
+    if gaps > 100-int(THRESHOLD):
         REMOVABLE_COLS.append(col)
 
 for col in reversed(REMOVABLE_COLS):
     ALIGN = ALIGN[:, :col-1] + ALIGN[:, col:]
 
-AlignIO.write(ALIGN, "./4_parsed_clustal_omega_results/muscle_uniref90_100.clw", "clustal")
+AlignIO.write(
+    ALIGN, "./4_parsed_clustal_omega_results/muscle_uniref90_100_{}.clw".format(THRESHOLD),
+    "clustal")
