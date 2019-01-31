4b4734c1753f0069e67a9b9d8bbf32919bfac93e
mac40 <marcobgg4@gmail.com>
2019-01-31 16:07:02 +0100

    manual work on clusters/refactor cd_hit_separator

--- Project/cd_hit_separator.py
+++ Project/cd_hit_separator.py
@@ -4,24 +4,32 @@ split clusters in single fasta files
 
 import re
 import sys
-import urllib.request
+# import urllib.request
 
-
-if __name__ == "__main__":
-    with open(sys.argv[1], 'r') as infile:
-        BASE_URL = 'https://www.uniprot.org/uniprot/'
-        EXTENSION = '.fasta'
+def cd_hit_separator(file):
+    '''
+    split cd-hit results in single clusters one for each file
+    '''
+    with open(file, 'r') as infile:
+        # base_url = 'https://www.uniprot.org/uniprot/'
+        # extension = '.fasta'
         for line in infile:
             if re.match(r'>Cluster\s[0-9]*', line):
                 if int(line[9:]) >= 1:
                     cluster.close()
                 cluster = open(
-                    'cd_hit_results/uniref90_100_90_against_homo_30/clusters/{}.fasta'.format(line[1:-1]), 'w')
+                    'cd_hit_results/uniref90_100_90_against_homo_30/clusters/{}.fasta'
+                    .format(line[1:-1]), 'w')
             else:
-                regex = re.search(r'[0-9]*\s[0-9]*aa,\s>[a-z]{2}\|(.*)\|.*', line)
+                regex = re.search(
+                    r'[0-9]*\s[0-9]*aa,\s>[a-z]{2}\|(.*)\|.*', line)
                 protein = regex.group(1)
-                url = BASE_URL + protein + EXTENSION
-                protein_fasta = urllib.request.urlopen(url).read()
+                # url = base_url + protein + extension
+                # protein_fasta = urllib.request.urlopen(url).read()
                 # cluster.write(protein_fasta.decode("utf-8"))
                 cluster.write('{}\n'.format(protein))
     cluster.close()
+
+
+if __name__ == "__main__":
+    cd_hit_separator(sys.argv[1])
